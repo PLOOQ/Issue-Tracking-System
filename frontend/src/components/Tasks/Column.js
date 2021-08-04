b79d80eb3eac994264e164a79391034077d4d225
Pablo Loo <PabloLoo1@Pablos-MacBook-Pro.local>
2021-08-04 16:02:10 -0500

    Static Column Labels

--- frontend/src/components/Tasks/Column.js
+++ frontend/src/components/Tasks/Column.js
@@ -5,14 +5,16 @@ const Column = (props) => {
   return (
     <div className="column">
       <div className="label">{props.label}</div>
-      {props.tasks.map((task) => (
-        <Card
-          key={task.id}
-          task={task}
-          openModalHandler={props.openModalHandler}
-          setDetailTask={props.setDetailTask}
-        ></Card>
-      ))}
+      <div className="column_cards">
+        {props.tasks.map((task) => (
+          <Card
+            key={task.id}
+            task={task}
+            openModalHandler={props.openModalHandler}
+            setDetailTask={props.setDetailTask}
+          ></Card>
+        ))}
+      </div>
     </div>
   );
 };
