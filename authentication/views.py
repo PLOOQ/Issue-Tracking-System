8d7022710a61a18271168760e96beb275bdeb316
Pablo Loo <pabloloo@aggienetwork.com>
2022-02-04 16:54:07 -0600

    Added login authentication view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+
+def my_view(request):
+    username = request.POST['pabloloo1']
+    password = request.POST['excruciatingly_long_password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        render("success.html")
+    else:
+        # Return an 'invalid login' error message.
+        return
