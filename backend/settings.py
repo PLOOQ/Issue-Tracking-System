0685f36da688766cd5892291a0a967823a7fcc79
Pablo Loo <pabloloo@aggienetwork.com>
2022-04-14 22:58:20 -0500

    Testing allow all CORS origins

--- backend/settings.py
+++ backend/settings.py
@@ -66,11 +66,12 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-    "https://mighty-issue-tracking-system.herokuapp.com"
-]
+CORS_ALLOW_ALL_ORIGINS: True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:3000",
+#     "http://localhost:3000",
+#     "https://mighty-issue-tracking-system.herokuapp.com"
+# ]
 
 ROOT_URLCONF = 'backend.urls'
 
